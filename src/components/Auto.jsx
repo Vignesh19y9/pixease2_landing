5a33aea97440534044b29fac69df4810611e0ebc
Vignesh <viwanathan.vignesh@heb.com>
2024-07-06 15:16:34 +0530

    Auto added.

--- src/components/Auto.jsx
+++ src/components/Auto.jsx
@@ -2,20 +2,25 @@ import React, { useState } from "react";
 import video1 from "../assets/DemoAutoCrop.mp4";
 const Auto = () => {
   return (
-    <div className="mt-20 ">
-      <h2 className="text-3xl sm:text-4xl lg:text-4xl text-center mt-6 tracking-wide font-bold">
-        Auto trim{" "}
-        <span className="bg-gradient-to-l from-pink-500 to-pink-500 text-transparent bg-clip-text font-light">
-          Auto color
+    <div className="m-20 space-y-4 p-20">
+      <h2 className="text-5xl text-center mt-6 tracking-wide font-bold">
+        Automatic{" "}
+        <span className="bg-gradient-to-l from-blue-700 to-blue-400 text-transparent bg-clip-text font-black">
+          Border{" "}
         </span>
+        Adjustment
       </h2>
-      <h2 className="text-lg  text-center tracking-wide ">
-        Easily cut out the excess blank areas from your screenshots. Easily cut
-        out the excess blank areas from your screenshots.
-      </h2>
+      <div>
+        <h2 className="text-lg  text-center tracking-wide ">
+          Easily cut out the excess blank areas from your screenshots.
+        </h2>
+        <h2 className="text-lg  text-center tracking-wide ">
+          Expand your borders with the screenshots background color.
+        </h2>
+      </div>
 
-      <div className="  bg-[#1a1a1a] rounded-3xl  flex flex-col lg:flex-row items-center mt-6 lg:mt-2 py-5">
-        <div className="flex flex-1 flex-col items-center">
+      <div className="  bg-gradient-to-r from-blue-400 to-blue-700 rounded-3xl  flex flex-col lg:flex-row items-center mt-6 lg:mt-2">
+        <div className="flex flex-1 flex-col items-center p-10">
           <Video videoSrc={video1} />
         </div>
       </div>
