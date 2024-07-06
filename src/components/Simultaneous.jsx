5dbcfbe84f53a06247e934b53072078879ae51c7
Vignesh <viwanathan.vignesh@heb.com>
2024-07-06 15:04:45 +0530

    Simultaneous added

--- src/components/Simultaneous.jsx
+++ src/components/Simultaneous.jsx
@@ -3,10 +3,10 @@ import video1 from "../assets/DemoApplyAll.mp4";
 
 const Simultaneous = () => {
   return (
-    <div className="mt-20 ">
-      <h2 className="text-3xl sm:text-4xl lg:text-4xl text-center mt-6 tracking-wide font-bold">
+    <div className="m-20 space-y-4 p-20">
+      <h2 className="text-5xl text-center mt-6 tracking-wide font-bold">
         Simultaneous{" "}
-        <span className="bg-gradient-to-l from-pink-500 to-pink-500 text-transparent bg-clip-text font-light">
+        <span className="bg-gradient-to-l from-purple-700 to-purple-400 text-transparent bg-clip-text font-black">
           Editing
         </span>
       </h2>
@@ -14,8 +14,8 @@ const Simultaneous = () => {
         Edit all your screenshots with just one control.
       </h2>
 
-      <div className="  bg-[#1a1a1a] rounded-3xl  flex flex-col lg:flex-row items-center mt-6 lg:mt-2 py-5">
-        <div className="flex flex-1 flex-col items-center">
+      <div className="  bg-gradient-to-r from-purple-400 to-purple-700 rounded-3xl  flex flex-col lg:flex-row items-center mt-6 lg:mt-2">
+        <div className="flex flex-1 flex-col items-center p-10">
           <Video videoSrc={video1} />
         </div>
       </div>
