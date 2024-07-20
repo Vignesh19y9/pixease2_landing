83ba85662139fa1a542d4134fb5c43815a4e8601
Vignesh <viwanathan.vignesh@heb.com>
2024-07-20 14:02:44 +0530

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,47 +5,44 @@ import { SocialIcon } from "react-social-icons";
 export default function App() {
   return (
     <footer className=" text-white dark:bg-neutral-910 p-10">
-      <div className="flex justify-end space-x-5 mb-5">
-        <div className="logo">
-          <SocialIcon
-            url="https://x.com/pixease_app"
-            bgColor="black"
-            fgColor="white"
-            style={{ width: 30, height: 30 }} // Adjust size as needed
-          />
+      <div className="flex-row ">
+        <div className="flex justify-center  lg:justify-start space-x-5 mb-5">
+          <div className="logo">
+            <SocialIcon
+              url="https://x.com/pixease_app"
+              bgColor="black"
+              fgColor="white"
+              style={{ width: 30, height: 30 }} // Adjust size as needed
+            />
+          </div>
+          <div className="logo">
+            <SocialIcon
+              url="https://www.linkedin.com/company/96591070"
+              bgColor="black"
+              fgColor="white"
+              style={{ width: 30, height: 30 }} // Adjust size as needed
+            />
+          </div>
         </div>
-        <div className="logo">
-          <SocialIcon
-            url="https://www.linkedin.com/company/96591070"
-            bgColor="black"
-            fgColor="white"
-            style={{ width: 30, height: 30 }} // Adjust size as needed
-          />
-        </div>
-      </div>
 
-      {/* Copyright, Privacy Policy, Terms of Use, and Email */}
-      <div className="flex justify-between items-center">
-        {/* Left side */}
-        <div className="flex space-x-2">
-          <p className="text-neutral-500" style={{ width: 120, height: 30 }}>
-            &copy; 2024 pixease
-          </p>
+        {/* Copyright, Privacy Policy, Terms of Use, and Email */}
+        <div className="flex flex-col lg:flex-row justify-between items-center space-y-2">
+          <div className="w-40">
+            <a href="mailto:contact@robzapps.com" className="text-neutral-500">
+              contact@robzapps.com
+            </a>
+          </div>
+
+          <div>
+            <p className="text-neutral-500 text-xs">&copy; 2024 pixease</p>
+          </div>
+
+          <div className="flex space-x-2 text-neutral-500 w-40 justify-center">
+            <Link to="/privacy">Privacy</Link>
+            <p>|</p>
+            <Link to="/terms">Terms</Link>
+          </div>
         </div>
-        {/* Center */}
-        <div className="flex space-x-2 text-neutral-500">
-          <Link to="/privacy">Privacy</Link>
-          <p>|</p>
-          <Link to="/terms">Terms</Link>
-        </div>
-        {/* Right side */}
-        <a
-          href="mailto:contact@robzapps.com"
-          className="text-neutral-500"
-          style={{ width: 140, height: 30 }}
-        >
-          contact@robzapps.com
-        </a>
       </div>
     </footer>
   );
